9bc4cf53f257819844b1903a7b8dabccf5d39817
Azathoth1729 <aa1203528272@gmail.com>
2021-11-11 20:44:20 +0800

    ignore build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/azathoth/backup/workshop/db/scudb_initial
-# Build directory: /home/azathoth/backup/workshop/db/scudb_initial/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/azathoth/workshop/db/scudb_initial
-# Build directory: /home/azathoth/workshop/db/scudb_initial/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
