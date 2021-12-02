77b30aa4a673b4514f9171e5802ee5511ee0ac25
Azathoth1729 <aa1203528272@gmail.com>
2021-12-02 20:55:05 +0800

    chore

--- src/index/b_plus_tree_index.cpp
+++ src/index/b_plus_tree_index.cpp
@@ -20,7 +20,7 @@ INDEX_TEMPLATE_ARGUMENTS
 void BPLUSTREE_INDEX_TYPE::InsertEntry(const Tuple &key, RID rid,
                                        Transaction *transaction) {
   // construct insert index key
-  KeyType index_key;
+  KeyType index_key{};
   index_key.SetFromKey(key);
 
   container_.Insert(index_key, rid, transaction);
@@ -30,7 +30,7 @@ INDEX_TEMPLATE_ARGUMENTS
 void BPLUSTREE_INDEX_TYPE::DeleteEntry(const Tuple &key,
                                        Transaction *transaction) {
   // construct delete index key
-  KeyType index_key;
+  KeyType index_key{};
   index_key.SetFromKey(key);
 
   container_.Remove(index_key, transaction);
@@ -40,7 +40,7 @@ INDEX_TEMPLATE_ARGUMENTS
 void BPLUSTREE_INDEX_TYPE::ScanKey(const Tuple &key, std::vector<RID> &result,
                                    Transaction *transaction) {
   // construct scan index key
-  KeyType index_key;
+  KeyType index_key{};
   index_key.SetFromKey(key);
 
   container_.GetValue(index_key, result, transaction);
